5efdf0d89494e3609b2ef8802d5b56ad6beb6976
Aleksandr Kretov <askretov@mail.ru>
2019-03-21 16:16:28 +1000

    go.mod added errors minor improvements

--- errors.go
+++ errors.go
@@ -3,23 +3,26 @@ package mutable
 import (
 	"errors"
 	"fmt"
+	"reflect"
 	"strings"
 )
 
 var (
 	errCannotSetValue = func(field string, value interface{}) error {
-		return fmt.Errorf("cannot set value (%s) to the field (%s)", value, field)
+		return fmt.Errorf("cannot set value (%v) to the field (%v)", value, field)
 	}
 	errCannotFind = func(field string) error {
-		return fmt.Errorf("cannot find a destination field (%s)", field)
+		return fmt.Errorf("cannot find a destination field (%v)", field)
 	}
 	errNotPointer       = errors.New("given value is not a Pointer type")
 	errNestedResetError = errors.New("cannot reset nested mutable object")
 	errNotSettable      = errors.New("field is not settable")
 	errNotInterfaceable = errors.New("field is not interfaceable")
 	errCannotParse      = errors.New("cannot parse value")
-	errUnsupportedType  = errors.New("unsupported value type")
-	errNotJSON          = errors.New("not a valid JSON value")
+	errUnsupportedType  = func(fieldType reflect.Type, value interface{}) error {
+		return fmt.Errorf("unsupported value type (%T) for a field (%v)", value, fieldType)
+	}
+	errNotJSON = errors.New("not a valid JSON value")
 )
 
 // IsCannotSetErr reports whether an err is a errCannotSetValue error
---
+++ go.mod
@@ -0,0 +1,9 @@
+module mutable
+
+require (
+	github.com/askretov/ansi v0.0.0-20150914162238-c286dcecd19f // indirect
+	github.com/askretov/mutable v0.0.0-20181008131038-20b36c167f58 // indirect
+	github.com/go-ext/logger v1.0.1
+	github.com/pquerna/ffjson v0.0.0-20181028064349-e517b90714f7
+	github.com/stretchr/testify v1.3.0
+)
